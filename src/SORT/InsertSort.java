29507a7311b458f0715c29aa431a702b4fd6d578
dixonshen <dixonshen@hotmail.com>
2016-04-08 11:54:56 +0800

    InsertSort

--- src/SORT/InsertSort.java
+++ src/SORT/InsertSort.java
@@ -1,36 +1,38 @@
 package SORT;
 
+import java.util.Arrays;
+
 /**
  * Created by DixonShen on 2016/4/8.
  */
 public class InsertSort {
 
     public static void main(String[] args) {
+        InsertSort ob = new InsertSort();
         int[] a = new int[100000];
         for (int i=0;i<100000;i++)
-            a[i] = (int)(10000000*Math.random());
+            a[i] = (int)(100000000*Math.random());
+        int[] b = Arrays.copyOf(a,a.length);
 //        for (int e:a) System.out.print(e + " ");
         System.out.println();
-        long startTime = System.currentTimeMillis();
-        insertSort(a);
-        long endTime = System.currentTimeMillis();
-        System.out.println("程序运行时间：" + (endTime - startTime) + "毫秒");
+        long startTime1 = System.currentTimeMillis();
+        ob.insertSort1(a);
+        long endTime1 = System.currentTimeMillis();
+        System.out.println("程序运行时间：" + (endTime1 - startTime1) + "毫秒");
 //        for (int e:a) System.out.print(e + " ");
     }
 
-    public static void insertSort(int a[]){
-        int temp;
-        for (int i=1;i<a.length;i++){
-            int p = i;
-            for (int k=i-1;k>=0;k--){
-                if(a[p]<a[k]){
-                    temp = a[k];
-                    a[k] = a[p];
-                    a[p] = temp;
-                    p--;
+    public void insertSort1(int a[]){
+        int i,j,temp;
+        for (i=1;i<a.length;i++) {
+            if (a[i]<a[i-1]) {
+                temp = a[i];
+                for (j = i - 1; j >= 0 && a[j]> temp; j--) {
+                    a[j+1] = a[j];
                 }
-                else break;
+                a[j + 1] = temp;
             }
         }
     }
+
 }
