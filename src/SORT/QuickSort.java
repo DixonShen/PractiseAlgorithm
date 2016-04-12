d5c114ceb3db80c4ec7b5fbdf249cfef7cb4a65d
dixonshen <dixonshen@hotmail.com>
2016-04-12 14:31:51 +0800

    QuickSort optimization not Arrays.sort()

--- src/SORT/QuickSort.java
+++ src/SORT/QuickSort.java
@@ -17,24 +17,24 @@ public class QuickSort {
 //        for (int e:a) System.out.print(e + " ");
         System.out.println();
         long startTime1 = System.currentTimeMillis();
-        ob.quickSort5(a, 0, a.length - 1);
-//        Arrays.sort(a);
+//        ob.quickSort6(a, 0, a.length - 1);
+        Arrays.sort(a);
         long endTime1 = System.currentTimeMillis();
         System.out.println("随机数组  " + (endTime1-startTime1));
         int[] b = new int[10000000];
         for (int i=0;i<b.length;i++)
             b[i] = i;
         long startTime2 = System.currentTimeMillis();
-        ob.quickSort5(b, 0, b.length - 1);
-//        Arrays.sort(b);
+//        ob.quickSort6(b, 0, b.length - 1);
+        Arrays.sort(b);
         long endTime2 = System.currentTimeMillis();
         System.out.println("升序数组  " + (endTime2-startTime2));
         int[] c = new int[10000000];
         for (int i=10000000,j=0;j<c.length;i--,j++)
             c[j] = i;
         long startTime3 = System.currentTimeMillis();
-        ob.quickSort5(c, 0, c.length - 1);
-//        Arrays.sort(c);
+//        ob.quickSort6(c, 0, c.length - 1);
+        Arrays.sort(c);
         long endTime3 = System.currentTimeMillis();
         System.out.println("降序数组  " + (endTime3-startTime3));
 
@@ -48,6 +48,7 @@ public class QuickSort {
         a[j] = temp;
     }
 
+    //median-of-three
     public static int med3(int x[], int a, int b, int c){
         int med;
         if (x[a]<x[b]) {
@@ -77,6 +78,11 @@ public class QuickSort {
         return med;
 //        return x[a] < x[b] ? ( x[b] < x[c] ? b : x[a] <x[c] ? c:a)
 //                : x[b] > x[c] ? b : x[a] > x[c] ? c : a;
+    }
+
+    public void vecswap(int x[], int a, int b, int n){
+        for (int i = 0;i<n;i++,a++,b++)
+            swap(x,a,b);
     }
 
     //快速排序基本实现
@@ -221,9 +227,6 @@ public class QuickSort {
             swap(a,l,m);
 
             int i = l, j = r + 1;
-            Random rand = new Random();
-            int p = rand.nextInt(r - l + 1) + l;
-            swap(a, i, p);
             int x = a[l];
             while (true) {
                 do {
@@ -237,8 +240,71 @@ public class QuickSort {
                 swap(a, i, j);
             }
             swap(a, l, j);
-            quickSort4(a, l, j - 1);
-            quickSort4(a, j + 1, r);
+            quickSort5(a, l, j - 1);
+            quickSort5(a, j + 1, r);
+        }
+    }
+
+    public void quickSort6(int a[], int l, int r){
+        if (l<r){
+
+            if((r-l+1)<7){
+                for (int i=l+1;i<=r;i++){
+                    if (a[i]<a[i-1]){
+                        int temp = a[i];
+                        int j = i-1;
+                        for (;j>=l&&a[j]>temp;j--)
+                            a[j+1] = a[j];
+                        a[j+1] = temp;
+                    }
+                }
+                return ;
+            }
+
+            int len = r-l+1;
+            int m = l + (len >> 1);
+            if (len >7){
+                int p = l;
+                int q = r;
+                if(len>40){
+                    int s = len/8;
+                    p = med3(a,p,p+s,p+2*s);
+                    m = med3(a,m-s,m,m+s);
+                    q = med3(a,q-2*s,q-s,q);
+                }
+                m = med3(a,p,q,m);
+            }
+
+
+            int p = l,q = p;
+            int s = r, t = s;
+            int x = a[m];
+            while (true){
+                while (p<=s&&a[p]<=x){
+                    if (a[p]==x)
+                        swap(a,q++,p);
+                    p++;
+                }
+                while (p<=s&&a[s]>=x){
+                    if (a[s]==x)
+                        swap(a,t--,s);
+                    s--;
+                }
+                if (p>s)
+                    break;
+                swap(a,p++,s--);
+            }
+
+            int i, n = l+len;
+            i = Math.min(q-l,p-q);
+            vecswap(a,l,p-i,i);
+            i = Math.min(t-s,n-1-t);
+            vecswap(a,p,n-i,i);
+
+            if ((i = p-q)>1)
+                quickSort6(a,l,l+i-1);
+            if ((i= t-s)>1)
+                quickSort6(a,r-i+1,r);
         }
     }
 }
