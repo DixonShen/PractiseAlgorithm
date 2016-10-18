ac9237220f6aad443b112a89b643626a548a3927
dixonshen <dixonshen@hotmail.com>
2016-10-18 11:43:13 +0800

    codewars Suqare into suqares

--- src/CodeWars/Decompose.java
+++ src/CodeWars/Decompose.java
@@ -10,29 +10,38 @@ public class Decompose {
 
     public static String decompose(long n){
         String result = "";
-        for (long i = n-1; i>=1; i--){
-            ArrayList<Long> tempResult = new ArrayList<Long>();
-            tempResult.add(i);
-            long temp = n*n - i*i;
-            for (long j = i; j>=1;){
-                j = (long) Math.sqrt(temp);
-                tempResult.add(j);
-                temp -= j*j;
-                if (temp == 0){
-                    return tempResult.toString();
-                }
-                if (temp == 1){
-                    tempResult.add(1L);
-                    return tempResult.toString();
-                }
-                if (temp == 2) break;
-                if (temp == 3) break;
-            }
+        long[] res = findSequence(n*n, n);
+        Arrays.sort(res);
+        for (long i : res){
+            result += i + " ";
         }
+        result = result.trim();
         return result;
     }
 
+    public static long[] findSequence(long n, long last){
+        long k = ((long)Math.sqrt(n) > last-1 ? last-1 : (long)Math.sqrt(n));
+        for ( ; k>=1; k--){
+            long temp = n - k * k;
+            if (temp == 0)
+                return new long[] {k};
+            if (temp == 2 || temp == 3)
+                continue;
+            if (temp == 1)
+                return new long[] {k,1L};
+            long[] res = findSequence(temp, k);
+            if (res == null) continue;
+            long[] result = new long[res.length + 1];
+            result[0] = k;
+            for (int i=1; i<result.length; i++){
+                result[i] = res[i-1];
+            }
+            return result;
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println(decompose(14));
+        System.out.println(decompose(12));
     }
 }
