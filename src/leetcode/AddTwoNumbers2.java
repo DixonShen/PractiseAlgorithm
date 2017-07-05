5960f6383130633cd9d924e0f26a592cad755d24
dixonshen <dixonshen@hotmail.com>
2017-07-05 17:06:36 +0800

    leetcode - 445. Add Two Numbers II (coding)

---
+++ src/leetcode/AddTwoNumbers2.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+/**
+ * Created by dixonshen on 2017/7/5.
+ */
+public class AddTwoNumbers2 {
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return  null;
+    }
+}
