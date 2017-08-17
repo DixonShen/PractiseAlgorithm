2eaa03fda258294ccc41540ef411e4d3311c455b
dixonshen <dixonshen@hotmail.com>
2017-08-17 17:16:18 +0800

    code interviews - 22. 栈的压入、弹出序列

--- src/Coding_Interviews/pop_and_push_order/Test22.java
+++ src/Coding_Interviews/pop_and_push_order/Test22.java
@@ -3,6 +3,7 @@ package Coding_Interviews.pop_and_push_order;
 import java.util.Stack;
 
 /**
+ * 输入两个整数序列，第一个序列表示栈的压入顺序，请判断二个序列是否为该栈的弹出顺序。假设压入栈的所有数字均不相等。
  * Created by dixonshen on 2017/8/17.
  */
 public class Test22 {
