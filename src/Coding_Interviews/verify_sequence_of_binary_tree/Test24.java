2b9c1486fc81c4089dad3370276471790b5a55af
dixonshen <dixonshen@hotmail.com>
2017-08-20 14:51:30 +0800

    code interviews - 24. 二叉搜索树的后序遍历序列

--- src/Coding_Interviews/verify_sequence_of_binary_tree/Test24.java
+++ src/Coding_Interviews/verify_sequence_of_binary_tree/Test24.java
@@ -2,6 +2,9 @@ package Coding_Interviews.verify_sequence_of_binary_tree;
 
 
 /**
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。如果是则返回true。否则返回false。假设输入的数组的任意两个数字都互不相同。
+ * 在后序遍历得到的序列中， 最后一个数字是树的根结点的值。
+ * 数组中前面的数字可以分为两部分： 第一部分是左子树结点的值，它们都比根结点的值小： 第二部分是右子树结点的值，它们都比根结点的值大。
  * Created by dixonshen on 2017/8/20.
  */
 public class Test24 {
